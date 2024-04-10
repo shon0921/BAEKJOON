821964df6d163c68c9dde42509e23199c8db48bd
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-04-10 22:21:04 +0900

    1110
    더하기 사이클

---
+++ Bronze I/1110.java
@@ -0,0 +1,24 @@
+package Other;
+
+import java.util.Scanner;
+
+public class Ch16 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int t = n;
+        int i = 0;
+
+        while(true) {
+            int a = t / 10;
+            int b = t % 10;
+            t = b * 10 + (a + b) % 10;
+            i++;
+            if(n == t) {
+                break;
+            }
+        }
+        System.out.println(i);
+    }
+}
