b0358b523a25b439d63bdca51610ae1a98dd035f
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-04-11 12:34:22 +0900

    2007년

---
+++ Bronze I/1924.java
@@ -0,0 +1,23 @@
+package Ch6;
+
+import java.util.Scanner;
+
+// 월을 x 일을 y로 선언할것
+public class T4 {
+    public static void main(String[] args) {
+        int [] dayx  = {31,28,31,30,31,30,31,31,30,31,30,31};
+        String [] dayy = {"SUN","MON", "TUE", "WED", "THU", "FRI", "SAT"} ;
+        Scanner s = new Scanner(System.in);
+        System.out.println("월 일 입력");
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+
+        int sum =0;
+        for (int i =0 ; i<n1-1 ;i++) {    //이전 월 전부 더하기
+            sum += dayx[i];
+        }
+        sum+=n2; // 현재 일 더하기
+
+        System.out.println(dayy[sum%7]);
+    }
+}
