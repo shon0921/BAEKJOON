df51fff766e50c60e4279a1647346281502a4bb1
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-04-12 08:59:51 +0900

    약수

---
+++ Bronze I/1037.java
@@ -0,0 +1,39 @@
+package Ch6;
+
+import java.util.Scanner;
+
+public class T4 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int A,N;
+        A = s.nextInt();    //약수 갯수
+        int i = 0;  //카운트
+        int [] arr = new int[50];
+        while (true)
+        {
+            arr[i] = s.nextInt(); //약수
+            i++;
+            if(i==A)
+            {
+                break;
+            }
+        }
+
+        int max = 0,min = 1000000; //최댓값 최솟값 선언
+
+        for (int j = 0;j<i;j++)
+        {
+            if(arr[j]>max)
+            {
+                max = arr[j];
+            }
+
+            if (arr[j]<min)
+            {
+                min = arr[j];
+            }
+        }
+        System.out.println(max*min);
+    }
+}
