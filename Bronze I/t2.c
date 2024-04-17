162ab59a1237cf5a51d2f57114c3627af6fc0c20
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-04-17 11:35:59 +0900

    Add files via upload

---
+++ Bronze I/t2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main()
+{
+	int arr[1001];
+
+	int num = 1;
+	int num1,num2;
+	int sum =0;
+
+	for(int i=1;i<=1000;i++)
+	{
+		for(int j = 1; j <= i ; j++)
+		{
+			arr[num]=i;
+			if(num>1000)
+			{
+				break;
+			}
+			num++;
+		}
+		
+	}
+
+	printf("ют╥б : ");
+	scanf("%d %d",&num1,&num2);
+
+	for(int i = num1; i <=num2; i++)
+	{
+		sum += arr[i];
+	}
+
+	printf("%d",sum);
+}
+	
+		
