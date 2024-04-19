1c027e79b1a9e3152aa3f5a543bb24e36a01623c
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-04-19 13:26:31 +0900

    Add files via upload

---
+++ Bronze I/B2748.java
@@ -0,0 +1,33 @@
+package Test;
+
+import java.util.Scanner;
+
+public class B2748 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n=s.nextInt();
+        long a = 0;
+        long b = 1;
+        int c;
+        long sum =0;
+
+        if(n==0)
+        {
+            sum = a;
+        } else if (n==1)
+        {
+            sum = b;
+        }
+        else
+        {
+            for(int i = 2; i<=n;i++)
+            {
+                sum = a + b;
+                a = b;
+                b = sum;
+            }
+        }
+        System.out.println(sum);
+    }
+}
