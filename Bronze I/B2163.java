0b6eda7febd1ee18fe91c969ca0c77a8506b5664
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-04-22 13:47:46 +0900

    Add files via upload

---
+++ Bronze I/B2163.java
@@ -0,0 +1,14 @@
+package Test;
+
+import java.util.Scanner;
+
+public class B2163 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N,M;
+        N = s.nextInt();
+        M = s.nextInt();
+        int sum = N*M-1;
+        System.out.println(sum);
+    }
+}
