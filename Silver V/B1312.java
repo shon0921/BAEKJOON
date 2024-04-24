03346a73e0949a5d80beede10a34db457fe44625
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-04-24 12:28:49 +0900

    Add files via upload

---
+++ Silver V/B1312.java
@@ -0,0 +1,23 @@
+package Test;
+
+import java.util.Scanner;
+
+public class B1312 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int A = s.nextInt();
+        int B = s.nextInt();
+        int N = s.nextInt();
+        int sum;
+        int num = A%B;
+
+        for (int i = 0; i < N-1; i++) {
+            num *= 10;
+            num %= B;
+        }
+        num *= 10;
+        System.out.print(num/B);
+        s.close();
+    }
+}
