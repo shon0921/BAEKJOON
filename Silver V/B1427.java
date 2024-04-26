83355fe411f3155774b5888d87b4e275fdb11f0e
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-04-26 09:54:11 +0900

    Add files via upload

---
+++ Silver V/B1427.java
@@ -0,0 +1,19 @@
+package Test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B1427 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        char[] arr = s.nextLine().toCharArray(); // 입력후 길이만큼 배열로 전환
+
+        Arrays.sort(arr); // 배열 오름차순 정렬
+
+        for (int i = arr.length-1; i>=0; i--) // 배열 내림차순으로
+        {
+            System.out.print(arr[i]);
+        }
+    }
+}
