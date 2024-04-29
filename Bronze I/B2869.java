288deba0306d101789409badadd528b02037dc1e
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-04-29 10:26:03 +0900

    Add files via upload

---
+++ Bronze I/B2869.java
@@ -0,0 +1,24 @@
+package Test;
+
+import java.util.Scanner;
+
+public class B2869 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int up = in.nextInt();		// A
+        int down = in.nextInt();	// B
+        int length = in.nextInt(); 	// C
+
+        int day = (length - down) / (up - down); // 총길이 - 내려갈 길이
+                                                 // 올라갈 길이 - 내려갈길이
+
+        // 나머지가 있을 경우
+        if ((length - down) % (up - down) != 0) { // 나머지가 있으면 하루 추가함
+            day++;
+        }
+        System.out.println(day);
+    }
+}
