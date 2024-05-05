6e1a9c83433ad5779c9274154f9598b76197c8b5
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-05-06 02:37:05 +0900

    Add files via upload

---
+++ Silver V/B9655.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class B9655 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int N = s.nextInt();
+
+        if(N%2==0)
+        {
+            System.out.println("CY");
+        }
+        else
+        {
+            System.out.println("SK");
+        }
+
+    }
+}
