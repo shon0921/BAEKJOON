d2206c7b3359be4dd486f2bd388377b9056a05d0
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-05-21 12:18:49 +0900

    Add files via upload

---
+++ Bronze I/B11557.java
@@ -0,0 +1,36 @@
+package Test;
+
+import java.util.Scanner;
+
+public class B11557 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int T = s.nextInt();
+        String S;
+        String Sr = "a";
+        int L;
+        int Lr = -1;
+        int N;
+
+        for (int i =0;i<T;i++)
+        {
+            N = s.nextInt();
+
+            for (int j = 0; j < N ; j++)
+            {
+                S = s.next();
+                L = s.nextInt();
+
+                if(L>Lr)
+                {
+                    Sr = S;
+                    Lr = L;
+                }
+            }
+            System.out.println(Sr);
+            Lr = -1;
+
+        }
+    }
+}
