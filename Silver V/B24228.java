d98cd4aab467a85a034339f105dcd406a4ab69e6
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-05-30 11:19:15 +0900

    Add files via upload

---
+++ Silver V/B24228.java
@@ -0,0 +1,14 @@
+package Test;
+
+import java.util.Scanner;
+
+public class B24228 {
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+
+        long N = s.nextLong();
+        long R = s.nextLong();
+
+        System.out.println(N+1+2*(R-1));
+    }
+}
