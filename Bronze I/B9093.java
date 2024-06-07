6ad7e4de531001ea5079ea66a9de4dace6c2f180
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-06-07 11:01:48 +0900

    Add files via upload

---
+++ Bronze I/B9093.java
@@ -0,0 +1,24 @@
+package Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B9093 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0 ; i<T;i++)
+        {
+            String [] s = br.readLine().split(" ");
+            for(String word : s)
+            {
+                StringBuffer sb = new StringBuffer(word);
+                System.out.print(sb.reverse().toString()+" ");
+            }
+            System.out.println();
+        }
+    }
+}
