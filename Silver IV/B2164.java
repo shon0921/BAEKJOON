a3a196448267774cfab4ada6fd315fe3e1865e20
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-06-11 20:25:57 +0900

    카드2

--- Silver IV/B2164.java
+++ Silver IV/B2164.java
@@ -10,10 +10,10 @@ public class Main {
         int N = s.nextInt();
         for(int i =1; i<= N; i++)
         {
-            q.offer(i); // 큐 생성
+            q.offer(i); // 큐 에 값 넣기?
         }
-        while (q.size()>1){ //큐가 1개 남을때까지 반복
-            q.poll(); // 맨앞 버리기
+        while (q.size()>1){ // 큐 값 안에 1개 남을때까지 반복
+            q.poll(); // 맨 앞 버리기
             q.offer(q.poll()); // 맨 앞 버리고 바로 뒤에 넣기
         }
         System.out.println(q.poll()); // 마지막 출력
