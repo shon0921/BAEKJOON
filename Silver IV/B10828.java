b30cd3e5f64dc9d0ae704a21b92afbdebace79c9
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-07-02 20:58:12 +0900

    스택

--- Silver IV/B10828.java
+++ Silver IV/B10828.java
@@ -13,26 +13,26 @@ public class Main {
             String S = s.next();
             switch (S) {
 
-                case "push" :
+                case "push" :                // 스택 값 넣기
                     stack.push(s.nextInt());
                     break;
-                case "pop" :
+                case "pop" :                //  스택 가장 위에있는 값 빼기
                     if(stack.empty()) {
                         System.out.println("-1");
                     } else {
                         System.out.println(stack.pop());
                     }
                     break;
-                case "size" :
+                case "size" :                // 스택 크기 출력
                     System.out.println(stack.size());
                     break;
-                case "empty" :
+                case "empty" :                //스택 비어 있으면 1 아니면 0
                     if (stack.empty()) {
                         System.out.println("1");
                     } else {
                         System.out.println("0");
                     } break;
-                case "top" :
+                case "top" :                //스택 가장 위에 있는 값 출력
                     if (stack.empty()) {
                         System.out.println("-1");
                     } else {
