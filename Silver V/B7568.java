70e2d5234753575784c354df73cd110bc1b2573d
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-07-30 23:43:19 +0900

    덩치

---
+++ Silver V/B7568.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner s = new Scanner(System.in);
+
+        int N = s.nextInt();
+        int[][] arr = new int [N][2];
+
+        for(int i=0;i<N;i++)
+        {
+            arr[i][0] = s.nextInt();
+            arr[i][1] = s.nextInt();
+        }
+
+        for(int i=0;i<N;i++){
+            int rank = 1;
+
+            for(int j=0;j<N;j++) {
+                if(i==j) continue;
+                if(arr[i][0]<arr[j][0] && arr[i][1] < arr[j][1]) {
+                    rank++;
+                }
+
+            }
+            System.out.print(rank+" ");
+        }
+    }
+}
