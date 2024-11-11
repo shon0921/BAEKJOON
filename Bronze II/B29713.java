521d2277e40aa29b5c4f04b1eba9ed803a33bde4
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-11-11 11:28:26 +0900

    브실이의 띠부띠부씰 컬렉션 🍪

--- Bronze II/B29713.java
+++ Bronze II/B29713.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String []args) {
         Scanner s = new Scanner(System.in);
-
+        //
         int A = s.nextInt();
         String M = s.next();
         int count = 0;
