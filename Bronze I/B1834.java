a75be754fa7ff9672fd53d100535b5c649198a80
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-02-23 21:42:24 +0900

    나머지와 몫이 같은 수

--- Bronze I/B1834.java
+++ Bronze I/B1834.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner s = new Scanner(System.in);
 		
-		long n = scanner.nextInt();
+		long n = s.nextInt();
 		long sum = 0;
-		for(int i=1; i<n; i++) sum += n*i+i;
+		for(int i=1; i<n; i++) {
+		sum += n*i+i;
+		}
 		System.out.print(sum);
-		scanner.close();
 	}
 }
