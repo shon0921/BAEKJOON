bacf3685a09837bb32d6b255392695f8d092ac08
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-08-21 10:30:01 +0900

    팩토리얼 2

---
+++ Brone V/B27433.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        long b = 1;
+
+
+        for (int i = 1; i <= a; i++) {
+            b*=i;
+        }
+        System.out.println(b);
+    }
+}
